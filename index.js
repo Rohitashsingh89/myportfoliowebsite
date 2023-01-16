a821e73e176024805fcc64ff5f7860659f9f37d8
Rohitash Singh <93479842+Rohitashsingh89@users.noreply.github.com>
2023-01-16 23:12:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// function thanks() {
+//     document.getElementById("submit").innerHTML = " ";
+//     // 'Thanks for submit your feedback'
+//     // submit.innerhtml='Hello world'
+//     // submit.disabled = true;
+// }
+
+const greeting = document.querySelector('.submit');
+greeting[0].innerHTML = "Thanks for your feedback";
