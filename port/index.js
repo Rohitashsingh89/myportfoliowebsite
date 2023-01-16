5baa9de0f45ad114c798455bd766d4f88d1629df
Rohitash Singh <93479842+Rohitashsingh89@users.noreply.github.com>
2023-01-16 23:12:54 +0530

    Delete port directory

--- port/index.js
+++
@@ -1,6 +0,0 @@
-function thanks() {
-    document.getElementById("submit").innerHTML = " ";
-    // 'Thanks for submit your feedback'
-    // submit.innerhtml='Hello world'
-    // submit.disabled = true;
-}
